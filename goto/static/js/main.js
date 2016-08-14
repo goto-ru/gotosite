09ef2f4bca12c5537d30d0945e1db987f26e5991
xenx <atvorozhkov@yandex.ru>
2016-08-14 19:09:55 +0300

    Added mobile navigation bar

--- goto/static/js/main.js
+++ goto/static/js/main.js
@@ -1,3 +1,11 @@
-$( document ).ready(function() {
-    if ($( document ).width()) < 700)
+$(document).ready(function () {
+    if ($(document).width() < 700) {
+        $('.todo, .time-col').css('margin', '0 auto');
+        $('.white-background, .chel').css('width', '100%');
+    }
+    $(".navbar-toggle").click(function () {
+        $('body').animate({right:'150'}, 500);
+        $('.mobile-sheet').animate({right:'0'}, 500);
+    });
+
 });
