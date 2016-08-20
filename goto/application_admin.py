c69546a05ce9293745d59eac921c4b98911cbc2e
Omrigan <omrigann@gmail.com>
2016-08-20 11:28:48 +0300

    Generic export

--- goto/application_admin.py
+++ goto/application_admin.py
@@ -15,29 +15,35 @@ from django.http import HttpResponse
 from wsgiref.util import FileWrapper
 from io import StringIO
 from django.contrib.admin import AdminSite
-import datetime
+import datetime, csv
 
 
 class ApplicationResource(resources.ModelResource):
     class Meta:
         model = Application
-        fields = ('participant__first_name', 'participant__last_name', 'participant__city', 'participant__birthday')
+        fields = tuple(('%s' % (f.name,) for f in Application._meta.get_fields())) + \
+                 tuple(('participant__%s' % (f.name,) for f in Participant._meta.get_fields()))
+        # fields = ('participant__first_name', 'participant__last_name', 'participant__city', 'participant__birthday')
+
+    def get_export_order(self):
+        # order = tuple(self._meta.export_order or ())
+        ret = self.current_fields
+        return ret
+        # return order + tuple(k for k in self.fields.keys() if k not in order)
 
 
-# admin.py
-class CountryFilter(admin.SimpleListFilter):
-    title = 'Class'  # or use _('country') for translated title
-    parameter_name = 'country'
+class GradeFilter(admin.SimpleListFilter):
+    title = 'Grade'
+    parameter_name = 'grade'
 
     def lookups(self, request, model_admin):
-        countries = set([c.country for c in model_admin.model.objects.all()])
-        return [(c.id, c.name) for c in countries]
-        # You can also use hardcoded model name like "Country" instead of
-        # "model_admin.model" if this is not direct foreign key filter
+        grades = [(i, str(i)) for i in range(1, 12)]
+        return grades
 
     def queryset(self, request, queryset):
         if self.value():
-            return queryset.filter(country__id__exact=self.value())
+            ids = [part.id for part in queryset.all() if part.participant.grade == self.value()]
+            return Participant.objects.filter(id__in=ids)
         else:
             return queryset
 
@@ -49,7 +55,7 @@ class ApplicationAdmin(SortableAdminMixin, ModelAdmin):
         event_name = queryset.get().arrangement.event.name
         send_mail('Подтверждение заявки на мероприятие "%s"' % event_name,
                   'Ваша заявка на мероприятие одобрена. Перейдите по ссылке <такой-то> чтобы подтвердить своё участие',
-                 settings.EMAIL_FROM, [_['participant__email'] for _ in queryset.values('participant__email')],
+                  settings.EMAIL_FROM, [_['participant__email'] for _ in queryset.values('participant__email')],
                   fail_silently=False)
 
     def reject(self, req, queryset):
@@ -66,23 +72,45 @@ class ApplicationAdmin(SortableAdminMixin, ModelAdmin):
                     args=[','.join([_['participant__email'] for _ in queryset.values('participant__email')])])
         )
 
-    def export_css(self, req, queryset):
-        s = StringIO()
-        s.write(ApplicationResource().export(queryset=queryset).csv)
-        s.seek(0)
-        response = HttpResponse(FileWrapper(s), content_type='application/csv')
-        response['Content-Disposition'] = datetime.datetime.now().strftime(
-            'attachment; filename=Applications-%d.%m.%y.csv')
-        return response
+    def export_generic(description="Export selected objects as CSV file", children_fields=None, file_format='csv',
+                       fields=None, exclude=None, header=True):
+        """
+        This function returns an export csv action
+        'fields' and 'exclude' work like in django ModelForm
+        'header' is whether or not to output the column names as the first row
+        """
 
-    def export_xlsx(self, req, queryset):
-        s = StringIO()
-        s.write(ApplicationResource().export(queryset=queryset).xlsx)
-        s.seek(0)
-        response = HttpResponse(FileWrapper(s), content_type='application/xlsx')
-        response['Content-Disposition'] = datetime.datetime.now().strftime(
-            'attachment; filename=Applications-%d.%m.%y.xlsx')
-        return response
+        def export(modeladmin, request, queryset):
+            """
+            Generic csv export admin action.
+            based on http://djangosnippets.org/snippets/1697/
+            """
+            opts = modeladmin.model._meta
+            field_names = set([field.name for field in opts.fields])
+            if children_fields:
+                for c in children_fields:
+                    child = getattr(modeladmin.model, c).field.model
+                    child_field_names = set(["%s__%s" % (c, field.name) for field in child._meta.fields])
+                    field_names |= child_field_names
+            if fields:
+                fieldset = set(fields)
+                field_names = field_names & fieldset
+            elif exclude:
+                excludeset = set(exclude)
+                field_names = field_names - excludeset
+
+            response = HttpResponse(mimetype='text/csv')
+            response['Content-Disposition'] = 'attachment; filename=%s.csv' % opts.replace('.', '_')
+
+            writer = csv.writer(response)
+            if header:
+                writer.writerow(list(field_names))
+            for obj in queryset:
+                writer.writerow([getattr(obj, field) for field in field_names])
+            return response
+
+        export.short_description = description
+        return export
 
     def current_age(self, obj):
         return obj.participant.current_age()
@@ -131,7 +159,9 @@ class ApplicationAdmin(SortableAdminMixin, ModelAdmin):
         'guy', 'city', 'current_age', 'stage', 'grade', 'date_sended', 'been',
         'status_admin')
 
-    list_filter = ('status', 'arrangement', 'participant__gender', 'participant__education_type')
+    list_filter = ('status', 'arrangement', 'participant__gender', 'participant__education_type',
+                   # GradeFilter
+                   )
     search_fields = ('participant__last_name', 'participant__first_name')
     list_display_links = tuple()
-    actions = [accept, reject, export_css, export_xlsx, send_custom_email]
+    actions = [accept, reject, export_csv, export_xlsx, send_custom_email]
