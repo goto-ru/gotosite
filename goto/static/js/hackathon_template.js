cf41de8cae0381e6a4f04063675991e8f6371a77
xenx <atvorozhkov@yandex.ru>
2016-08-30 16:35:24 +0300

    Now logo in hackathon template looks not like Oleg when he eat his breakfast

--- goto/static/js/hackathon_template.js
+++ goto/static/js/hackathon_template.js
@@ -3,6 +3,8 @@ function include(arr, obj) {
 }
 
 $(document).ready(function () {
+    // Fucking firefox fix
+    $('#logo').css('max-height',$('#text-near-logo').css('height'));
     var a = 0;
     $('.grey-colored').each(function () {
         a += 1;
